d1ed34a591dccb8c735f6a4eb53a791e6edbc958
kapilsaini3394 <hjddsaf76@outlook.com>
2024-11-23 01:31:24 -0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+```
+#include <iostream>
+
+int main() {
+    // Creating an array of integers
+    int arr[5] = {10, 20, 30, 40, 50};
+
+    // Accessing and printing elements using an index
+    std::cout << "Element at index 0: " << arr[0] << std::endl;
+    std::cout << "Element at index 1: " << arr[1] << std::endl;
+    std::cout << "Element at index 2: " << arr[2] << std::endl;
+
+    return 0;
+}
+```
